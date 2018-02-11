00d3fbad03640ea2d47fcb868d77eae168995035
j- <j@skeoh.com>
2018-02-12 00:28:06 +1100

    Add rainbow button group story

--- stories/components/ButtonGroup.jsx
+++ stories/components/ButtonGroup.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Button, ButtonGroup } from '../../src';
+import { ThemeProvider } from '../../src/theme';
 
 storiesOf('ButtonGroup', module)
 	.add('default', () => (
@@ -17,5 +18,45 @@ storiesOf('ButtonGroup', module)
 			<Button type="button" intent="primary">Bar</Button>
 			<Button type="button" intent="primary" disabled={true}>Qux</Button>
 			<Button type="button" intent="primary">Baz</Button>
+		</ButtonGroup>
+	))
+	.add('rainbow', () => (
+		<ButtonGroup>
+			<ThemeProvider theme={{ primaryColor: 'hsl(0, 80%, 60%)' }}>
+				<Button type="button" intent="primary">Button</Button>
+			</ThemeProvider>
+			<ThemeProvider theme={{ primaryColor: 'hsl(30, 80%, 60%)' }}>
+				<Button type="button" intent="primary">Button</Button>
+			</ThemeProvider>
+			<ThemeProvider theme={{ primaryColor: 'hsl(60, 80%, 60%)' }}>
+				<Button type="button" intent="primary">Button</Button>
+			</ThemeProvider>
+			<ThemeProvider theme={{ primaryColor: 'hsl(90, 80%, 60%)' }}>
+				<Button type="button" intent="primary">Button</Button>
+			</ThemeProvider>
+			<ThemeProvider theme={{ primaryColor: 'hsl(120, 80%, 60%)' }}>
+				<Button type="button" intent="primary">Button</Button>
+			</ThemeProvider>
+			<ThemeProvider theme={{ primaryColor: 'hsl(150, 80%, 60%)' }}>
+				<Button type="button" intent="primary">Button</Button>
+			</ThemeProvider>
+			<ThemeProvider theme={{ primaryColor: 'hsl(180, 80%, 60%)' }}>
+				<Button type="button" intent="primary">Button</Button>
+			</ThemeProvider>
+			<ThemeProvider theme={{ primaryColor: 'hsl(210, 80%, 60%)' }}>
+				<Button type="button" intent="primary">Button</Button>
+			</ThemeProvider>
+			<ThemeProvider theme={{ primaryColor: 'hsl(240, 80%, 60%)' }}>
+				<Button type="button" intent="primary">Button</Button>
+			</ThemeProvider>
+			<ThemeProvider theme={{ primaryColor: 'hsl(270, 80%, 60%)' }}>
+				<Button type="button" intent="primary">Button</Button>
+			</ThemeProvider>
+			<ThemeProvider theme={{ primaryColor: 'hsl(300, 80%, 60%)' }}>
+				<Button type="button" intent="primary">Button</Button>
+			</ThemeProvider>
+			<ThemeProvider theme={{ primaryColor: 'hsl(330, 80%, 60%)' }}>
+				<Button type="button" intent="primary">Button</Button>
+			</ThemeProvider>
 		</ButtonGroup>
 	));
